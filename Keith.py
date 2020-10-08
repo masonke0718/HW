086cbc23841d3d28e977f5c3dffebeeeb25efc13
masonke0718 <72037412+masonke0718@users.noreply.github.com>
2020-10-08 09:13:43 -0500

    Add files via upload

---
+++ Keith.py
@@ -0,0 +1,19 @@
+# Exercise #2
+
+def printSquareFtg(sqft):
+
+    # calculate the square footage 
+    sqft = length * width
+    return f'The square footag of the house is: {sqft}'
+
+    print(f'The square footage of the the house is: {sqft}')
+    
+print('\n')
+
+rad = input("Enter radius of circle: ");
+if rad == 'x':
+    exit();
+else:
+    radius = (rad);
+    circumference = 2*3.14*radius;
+    print(f'Circumference of Circle =',circumference);
